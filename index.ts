e5018ee39e17099375608b545abca88cb1be5145
Simonas Jončys <simonas.joncys@trafi.com>
2020-01-20 11:48:21 +0200

    Do not add #undefined to the end of URL after redirect
    This checks to see if the redirect URL actually had a hash URL before trying
    to append it to the cleaned-up URL.

--- index.ts
+++ index.ts
@@ -580,7 +580,7 @@ export class OAuth2AuthCodePKCE {
     const [urlPart, queryStringAndHashUrlPart] = url.split('?');
     // @ts-ignore
     const [_queryParamsPart, hashUrlPart] = queryStringAndHashUrlPart.split('#');
-    return `${urlPart}#${hashUrlPart}`;
+    return `${urlPart}${hashUrlPart !== undefined ? `#${hashUrlPart}` : ''}`;
   }
 
   /**
